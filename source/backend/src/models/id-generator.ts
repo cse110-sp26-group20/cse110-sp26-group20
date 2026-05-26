e179c7a57b95ed7d0bb8536e68f46693e6815c01
Microzenas <38436162+microzen@users.noreply.github.com>
2026-05-25 20:47:16 -0700

    Add abstract classes and interfaces to file system (#56)

---
+++ source/backend/src/models/id-generator.ts
@@ -0,0 +1,14 @@
+export interface IDGenerator {
+  generate(): string;
+}
+
+export class CounterGenerator implements IDGenerator {
+  count: number;
+  constructor() {
+    this.count = 0;
+  }
+  generate(): string {
+    this.count++;
+    return this.count.toString();
+  }
+}
