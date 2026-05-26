02b62450c1c84a55d4ebf4fbf6df1e470d248c39
Microzenas <microzenas@gmail.com>
2026-05-25 17:18:10 -0700

    Add abstract classes and interfaces to the storage strategy, file system, and ID generator models.

---
+++ source/backend/src/models/file-storage.ts
@@ -0,0 +1,29 @@
+import { Readable, type Stream } from 'stream';
+
+export abstract class StorageStrategy {
+  /**
+   * 
+   * @param filename name
+   * @param fileStream stream
+   * @return path/filename.ext
+   */
+  abstract write(filename: string, fileStream: Buffer | Stream): string;
+  abstract read(filename: string): Stream;
+  abstract remove(filename: string): boolean;
+  abstract resolvePath(filename: string): string;
+}
+
+export class NoStorageStrategy implements StorageStrategy {
+  write(): string {
+    return '';
+  }
+  read(): Stream {
+    return Readable.from([]);
+  }
+  remove(): boolean {
+    return false;
+  }
+  resolvePath(): string {
+    return '';
+  }
+}
---
+++ source/backend/src/models/file-system.ts
@@ -0,0 +1,26 @@
+import type Stream from 'stream';
+
+import type { StorageStrategy } from './file-storage';
+
+export class FileRecord {
+  constructor(
+    public id: string,
+    public filename: string,
+    public localPath: string,
+    /**
+     * MIME Type (image/png, image/jpeg, application/json, and etc.)
+     */
+    public type: string,
+    public create: Date,
+    public matedata: Record<string,unknown>
+  ) {}
+}
+export interface FileRepositoryOperator {
+  saveFile(
+    file: Buffer | Stream,
+    metadata: Partial<FileRecord>,
+    strategy: StorageStrategy
+  ): FileRecord;
+  getFileById(id: string): FileRecord | undefined;
+  getFileStream(id: string, strategy: StorageStrategy): Stream | undefined;
+}
