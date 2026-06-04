660ba1a80de3a43d4f0c14dc3c485e540e5999d4
Microzenas <microzenas@gmail.com>
2026-06-03 17:22:17 -0700

    Implement ImageController and associated router with error handling and tests

---
+++ source/backend/src/controllers/image.controller.ts
@@ -0,0 +1,37 @@
+import type { NextFunction, Request, Response } from 'express';
+
+import type { StorageStrategy } from '../models/file-storage';
+import type { FileRepositoryOperator } from '../models/file-system';
+
+export class ImageController {
+  fileRepo: FileRepositoryOperator;
+  stratage: StorageStrategy;
+  constructor(fileRepo: FileRepositoryOperator, stratage: StorageStrategy) {
+    this.fileRepo = fileRepo;
+    this.stratage = stratage;
+  }
+  async getImg(req: Request, resp: Response, nextFunc: NextFunction) {
+    try {
+      const imageID = req.params.id;
+      if (!imageID) {
+        return resp.status(400).json({ error: 'no image id on the params.' });
+      }
+      const record = this.fileRepo.getFileById(imageID as string);
+
+      if (!record) {
+        return resp.status(404).json({ error: 'Image not found.' });
+      }
+
+      const imageUrl =
+        record.localPath || (await this.stratage.resolvePath(record.filename));
+
+      return resp.status(200).json({
+        id: record.id,
+        url: imageUrl
+      });
+    } catch (error) {
+      console.error('Failed to get the picture:', error);
+      nextFunc(error);
+    }
+  }
+}
---
+++ source/backend/src/tests/get-image-api.test.ts
@@ -0,0 +1,133 @@
+import type { NextFunction, Request, Response } from 'express';
+
+import { ImageController } from '../controllers/image.controller';
+import { StorageStrategy } from '../models/file-storage';
+import type { FileRecord, FileRepositoryOperator } from '../models/file-system';
+
+describe('ImageController', () => {
+  let mockFileRepo: jest.Mocked<FileRepositoryOperator>;
+  let mockStorageStrategy: jest.Mocked<StorageStrategy>;
+  let imageController: ImageController;
+
+  let mockRequest: Partial<Request>;
+  let mockResponse: Partial<Response>;
+  let mockNext: jest.MockedFunction<NextFunction>;
+
+  beforeEach(() => {
+    mockFileRepo = {
+      saveFile: jest.fn(),
+      getFileById: jest.fn(),
+      getFileStream: jest.fn()
+    };
+    mockStorageStrategy = {
+      resolvePath: jest.fn(),
+      read: jest.fn(),
+      write: jest.fn(),
+      remove: jest.fn()
+    };
+
+    imageController = new ImageController(mockFileRepo, mockStorageStrategy);
+
+    mockRequest = {
+      params: {}
+    };
+
+    mockResponse = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+
+    mockNext = jest.fn();
+
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  test('if params has not id, return 400', async () => {
+    mockRequest.params = {};
+
+    await imageController.getImg(
+      mockRequest as Request,
+      mockResponse as Response,
+      mockNext
+    );
+
+    expect(mockResponse.status).toHaveBeenCalledWith(400);
+    expect(mockResponse.json).toHaveBeenCalledWith({
+      error: 'no image id on the params.'
+    });
+
+    expect(mockFileRepo.getFileById).not.toHaveBeenCalled();
+    expect(mockStorageStrategy.resolvePath).not.toHaveBeenCalled();
+  });
+
+  test('if not at file repo, return 404', async () => {
+    mockRequest.params = { id: '999' };
+    mockFileRepo.getFileById.mockReturnValue(undefined);
+
+    await imageController.getImg(
+      mockRequest as Request,
+      mockResponse as Response,
+      mockNext
+    );
+
+    expect(mockFileRepo.getFileById).toHaveBeenCalledWith('999');
+    expect(mockResponse.status).toHaveBeenCalledWith(404);
+    expect(mockResponse.json).toHaveBeenCalledWith({
+      error: 'Image not found.'
+    });
+  });
+  test('if existed a localPath, the localPath should be return with no stratage call.', async () => {
+    mockRequest.params = { id: '1' };
+    const mockRecord: FileRecord = {
+      id: '1',
+      filename: 'avatar.png',
+      localPath: '/static/avatar.png',
+      type: '',
+      create: new Date(),
+      matedata: {}
+    };
+    mockFileRepo.getFileById.mockReturnValue(mockRecord);
+    await imageController.getImg(
+      mockRequest as Request,
+      mockResponse as Response,
+      mockNext
+    );
+    expect(mockResponse.status).toHaveBeenCalledWith(200);
+    expect(mockResponse.json).toHaveBeenCalledWith({
+      id: '1',
+      url: '/static/avatar.png'
+    });
+    expect(mockStorageStrategy.resolvePath).not.toHaveBeenCalled();
+  });
+
+  test('if NOT a localPath is existed, stratage should be called.', async () => {
+    mockRequest.params = { id: '1' };
+    const mockRecord: FileRecord = {
+      id: '1',
+      filename: 'avatar.png',
+      localPath: '',
+      type: '',
+      create: new Date(),
+      matedata: {}
+    };
+    mockStorageStrategy.resolvePath.mockResolvedValue('/static/avatar.png');
+    mockFileRepo.getFileById.mockReturnValue(mockRecord);
+    await imageController.getImg(
+      mockRequest as Request,
+      mockResponse as Response,
+      mockNext
+    );
+    expect(mockResponse.status).toHaveBeenCalledWith(200);
+    expect(mockResponse.json).toHaveBeenCalledWith({
+      id: '1',
+      url: '/static/avatar.png'
+    });
+    expect(mockStorageStrategy.resolvePath).toHaveBeenCalledWith(
+      mockRecord.filename
+    );
+  });
+});
