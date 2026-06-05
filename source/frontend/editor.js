e23e772b8922a91ff1df5384d2f315172a42a725
Jad Isaac <jadjrisaac@gmail.com>
2026-06-04 19:18:06 -0700

    feat: add top and bottom text

--- source/frontend/editor.js
+++ source/frontend/editor.js
@@ -4,6 +4,35 @@ const context = canvas.getContext('2d');
 
 const savedImage = sessionStorage.getItem('uploadedImage');
 
+// text captions
+let currentImage = null;
+
+const topCaption = {
+    text: "",
+    x: 0,
+    y: 0
+};
+
+const bottomCaption = {
+    text: "",
+    x: 0,
+    y: 0
+};
+
+// grabbing inputs from html
+const topInput = document.getElementById("top-text");
+const bottomInput = document.getElementById("bottom-text");
+
+topInput.addEventListener("input", (event) => {
+    topCaption.text = event.target.value;
+    renderCanvas();
+});
+
+bottomInput.addEventListener("input", (event) => {
+    bottomCaption.text = event.target.value;
+    renderCanvas();
+});
+
 if (savedImage) {
     const img = new Image();
     img.src = savedImage; //assigns chosen image to image element & decodes base64 url
@@ -12,7 +41,61 @@ if (savedImage) {
     img.onload = () => {
         canvas.width = img.width;
         canvas.height = img.height;
-        context.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+        topCaption.x = canvas.width / 2;
+        topCaption.y = 80;
+
+        bottomCaption.x = canvas.width / 2;
+        bottomCaption.y = canvas.height - 40;
+
+        renderCanvas();
         sessionStorage.removeItem('uploadedImage');
     };
+}
+
+function renderCanvas() {
+
+    context.clearRect(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+
+    if (currentImage) {
+        context.drawImage(
+            currentImage,
+            0,
+            0,
+            canvas.width,
+            canvas.height
+        );
+    }
+
+    drawCaption(topCaption);
+    drawCaption(bottomCaption);
+}
+
+function drawCaption(caption) {
+
+    context.font = "bold 60px Impact";
+
+    context.fillStyle = "white";
+    context.strokeStyle = "black";
+
+    context.lineWidth = 4;
+
+    context.textAlign = "center";
+
+    context.strokeText(
+        caption.text,
+        caption.x,
+        caption.y
+    );
+
+    context.fillText(
+        caption.text,
+        caption.x,
+        caption.y
+    );
 }
