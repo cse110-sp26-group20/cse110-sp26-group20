904a33b5609e67bff1e8443ee55dc3da3ee00200
Cadie Sparks <cadiesparks@gmail.com>
2026-06-07 22:09:42 -0700

    Add jest/puppeteer globals to frontend eslint

--- source/frontend/eslint.config.js
+++ source/frontend/eslint.config.js
@@ -5,12 +5,29 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 
-export default defineConfig(js.configs.recommended, eslintConfigPrettier, {
-  files: ['**/*.js'],
-  ignores: ['node_modules/**'],
-  languageOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    globals: globals.browser
+export default defineConfig(
+  js.configs.recommended,
+  eslintConfigPrettier,
+  {
+    // App
+    files: ['**/*.js'],
+    ignores: ['node_modules/**'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser
+    }
+  },
+  // Jest + puppeteer tests
+  {
+    files: ['tests/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+        page: 'readonly'
+      }
+    }
   }
-});
+);
